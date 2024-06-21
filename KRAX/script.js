1585e89c683a9ddbe17b8ed72dc55db327911ceb
KRACKqwerty <152689225+KRACKqwerty@users.noreply.github.com>
2024-06-21 19:26:34 +0300

    Add files via upload

--- KRAX/script.js
+++ KRAX/script.js
@@ -1,3 +1,4 @@
+AOS.init()
 let button = document.querySelector('.mode-button')
 function toggle_mode() {
     let is_dark_mode = localStorage.getItem('dark_mode')
@@ -10,6 +11,16 @@ function toggle_mode() {
     document.querySelector('header').classList.toggle('head-dark-mode',is_dark_mode)
     document.querySelector('main').classList.toggle('main-dark-mode',is_dark_mode)
     document.querySelector('footer').classList.toggle('footer-dark-mode',is_dark_mode)
+    
+    document.querySelectorAll('a').forEach(element => {
+        element.classList.toggle('a-dark-mode',is_dark_mode)
+    });
+    
+    document.querySelector('.mode-button').classList.toggle('mode-button-dark-mode',is_dark_mode)
+    document.querySelector('.mode-button:active').classList.toggle('mode-button-dark-mode:active',is_dark_mode)
+    document.querySelector('.mode-button:hover').classList.toggle('mode-button-dark-mode:hover',is_dark_mode)
+    document.querySelector('a:hover').classList.toggle('a-dark-mode:hover',is_dark_mode)
+    document.querySelector('a:active').classList.toggle('a-dark-mode:active',is_dark_mode)
 
 }
 button.addEventListener('click',toggle_mode)
